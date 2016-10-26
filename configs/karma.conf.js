nikhilmetrani/beta-ori

commit 8eb9e495f92d13ff123c865468fbfc7225de214b

Implementation for items #10, #11 and #12
Important:
This change requires clean-all and install to be executed.
After pulling this change, run following commands -

npm run clean-all
npm install

1. #10 Support for webpack:
   webpack configs for dev, test and production environments have been added.
   To run, use the command: npm start
2. #11 Client side JWT:
   This has been implemented by adding a JsonHttp wrapper around the Http class.
   Request for JsonHttp in your service instead of Angular's Http.
3. #12 Login and signup pages have been added. Note: the server side routes
   must be moved under /api/** route for this item to work. Subsequent commits
   will implement this change.
4. Added several unit test cases and protractor along with e2e has been dropped.
   All testing is now covered using jasmine framework.

configs/karma.conf.js
@@ -1,0 +1,42 @@
+var ENV = process.env.npm_lifecycle_event;
+var isTestWatch = ENV === 'test-watch';
+
+module.exports = function (config) {
+  var testWebpackConfig = require('./webpack.test.js');
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    exclude: [],
+    files: [
+      {pattern: './configs/spec-bundle.js', watched: false}
+    ],
+    preprocessors: {
+      './configs/spec-bundle.js': ['webpack', 'sourcemap']
+    },
+    webpack: testWebpackConfig,
+    webpackMiddleware: {
+      // webpack-dev-middleware configuration
+      // i. e.
+      stats: 'errors-only'
+    },
+    webpackServer: {
+      noInfo: true
+    },
+    reporters: isTestWatch ? ['mocha']: ['mocha', 'coverage'],
+    coverageReporter: isTestWatch ? undefined : {
+      dir: 'coverage/',
+      reporters: [{
+        type: 'json',
+        dir: 'coverage',
+        subdir: 'json',
+        file: 'coverage-final.json'
+      }]
+    },
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: isTestWatch ? ['Chrome'] : ['PhantomJS'], 
+    singleRun: true
+   });
+};

configs/webpack.test.js
@@ -1,0 +1,67 @@
+const helpers = require('./helpers');
+const webpack = require('webpack');
+
+const ENV = process.env.ENV = process.env.NODE_ENV = 'test';
+
+module.exports = {
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: ['.ts', '.js'],
+    alias: {
+      "lodash": "lodash-es",
+    },
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.ts$/,
+        loader: 'tslint-loader',
+        exclude: [helpers.root('node_modules')]
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader',
+        exclude: [
+          helpers.root('node_modules/rxjs'),
+          helpers.root('node_modules/@angular')
+        ]
+      },
+      {
+        test: /\.ts$/,
+        loaders: ['awesome-typescript-loader?inlineSourceMap=true&sourceMap=false', 'angular2-template-loader']
+      },
+      {test: /\.css$/, loader: 'raw-loader'},
+      {test: /\.html$/, loader: 'raw-loader'},
+      // {
+      //   test: /\.ts$/,
+      //   include: helpers.path.resolve('src'),
+      //   loader: 'istanbul-instrumenter-loader',
+      //   exclude: [/\.spec\.ts$/, /\.e2e\.ts$/, /node_modules/]
+      // },
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'ENV': JSON.stringify(ENV),
+    }),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        tslint: {
+          emitErrors: false,
+          failOnHint: false,
+          resourcePath: 'src'
+        },
+      },
+    }),
+  ],
+  node: {
+    global: true,
+    crypto: 'empty',
+    process: false,
+    module: false,
+    clearImmediate: false,
+    setImmediate: false
+  }
+};

src/app/components/signup/signup.component.spec.ts
@@ -1,0 +1,114 @@
+import {Component, DebugElement} from '@angular/core';
+import {Location} from '@angular/common';
+import {By} from '@angular/platform-browser/src/dom/debug/by';
+import {
+  inject,
+  fakeAsync,
+  TestBed,
+  ComponentFixture
+} from '@angular/core/testing';
+import {BaseResponseOptions, Response} from '@angular/http';
+import {MockBackend} from '@angular/http/testing';
+import {Router} from '@angular/router';
+import {SignupComponent} from './signup.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {LoginService} from '../../core/services/login.service';
+import {BOCoreModule} from '../../core';
+import {SignupModule} from './signup.module';
+import {APP_TEST_HTTP_PROVIDERS, advance} from '../../../testing';
+
+describe('SignupComponent', () => {
+
+  @Component({
+    template: `<bo-signup></bo-signup><router-outlet></router-outlet>`,
+  })
+  class TestComponent {
+  }
+
+  @Component({
+    template: ``,
+  })
+  class BlankComponent {
+  }
+
+  let fixture: ComponentFixture<any>;
+  let cmpDebugElement: DebugElement;
+
+  let loginService: LoginService;
+  let backend: MockBackend;
+  let router: Router;
+  let location: Location;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule.withRoutes([
+          {
+            path: 'home',
+            component: BlankComponent,
+          },
+        ]),
+        BOCoreModule,
+        SignupModule,
+      ],
+      providers: [
+        APP_TEST_HTTP_PROVIDERS,
+      ],
+      declarations: [
+        TestComponent,
+        BlankComponent,
+      ]
+    });
+  });
+  beforeEach(inject([LoginService, MockBackend, Router, Location], (..._) => {
+    [loginService, backend, router, location] = _;
+  }));
+  beforeEach(fakeAsync(() => {
+    TestBed.compileComponents().then(() => {
+      fixture = TestBed.createComponent(TestComponent);
+      fixture.detectChanges();
+      cmpDebugElement = fixture.debugElement.query(By.directive(SignupComponent));
+    });
+  }));
+
+  it('can be shown', () => {
+    expect(cmpDebugElement).toBeTruthy();
+  });
+
+  it('can validate inputs', () => {
+    const page: SignupComponent = cmpDebugElement.componentInstance;
+    page.username.setValue('a');
+    page.firstname.setValue('fn');
+    page.lastname.setValue('ln');
+    page.email.setValue('b', {});
+    page.password.setValue('c', {});
+    page.passwordConfirmation.setValue('d', {});
+    expect(page.myForm.valid).toBeFalsy();
+    page.username.setValue('akira', {});
+    page.firstname.setValue('fn');
+    page.lastname.setValue('ln');
+    page.email.setValue('test@test.com', {});
+    page.password.setValue('secret123', {});
+    page.passwordConfirmation.setValue('secret123', {});
+    expect(page.myForm.valid).toBeTruthy();
+  });
+
+  it('can signup', fakeAsync(() => {
+    const page: SignupComponent = cmpDebugElement.componentInstance;
+    spyOn(loginService, 'login').and.callThrough();
+    backend.connections.subscribe(conn => {
+      conn.mockRespond(new Response(new BaseResponseOptions()));
+    });
+    page.onSubmit({
+      email: 'test@test.com',
+      password: 'secret',
+      username: 'akira',
+      firstname: 'fn',
+      lastname: 'ln'
+    });
+    expect(loginService.login).toHaveBeenCalledWith('akira', 'secret');
+    advance(fixture);
+    expect(location.path()).toEqual('/');
+  }));
+
+});

karma.conf.js
@@ -1,87 +1,1 @@
-var path = require('path');
-
-var webpackConfig = require('./webpack.config');
-
-var ENV = process.env.npm_lifecycle_event;
-var isTestWatch = ENV === 'test-watch';
-
-module.exports = function (config) {
-  var _config = {
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
-
-    // list of files / patterns to load in the browser
-    files: [
-      { pattern: './karma-shim.js', watched: false }
-    ],
-
-    // list of files to exclude
-    exclude: [],
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      './karma-shim.js': ['webpack', 'sourcemap']
-    },
-
-    webpack: webpackConfig,
-
-    webpackMiddleware: {
-      // webpack-dev-middleware configuration
-      // i. e.
-      stats: 'errors-only'
-    },
-
-    webpackServer: {
-      noInfo: true // please don't spam the console when running in karma!
-    },
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress', 'mocha'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ["mocha"],
-
-    // web server port
-    port: 9876,
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: isTestWatch ? ['Chrome'] : ['PhantomJS'], 
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
-  };
-
-  if (!isTestWatch) {
-    _config.reporters.push("coverage");
-
-    _config.coverageReporter = {
-      dir: 'coverage/',
-      reporters: [{
-        type: 'json',
-        dir: 'coverage',
-        subdir: 'json',
-        file: 'coverage-final.json'
-      }]
-    };
-  }
-
-  config.set(_config);
-
-};
+module.exports = require('./configs/karma.conf.js');

commit 458252f28fb05971fa47328fecb09b8e0d7b2cac

Fix for build break caused by commit 8eb9e495f9
1. Updated the 'can validate inputs' unit test by changing the value
   of first and last name fields to 4+ characters in length.
2. Removed pretest execution of lint as it is handled by npm test.
3. Removed unwanted configuration from karma.conf.js, this was carried over
   from previous test setup.

configs/karma.conf.js
@@ -1,5 +1,4 @@
 var ENV = process.env.npm_lifecycle_event;
-var isTestWatch = ENV === 'test-watch';
 
 module.exports = function (config) {
   var testWebpackConfig = require('./webpack.test.js');
@@ -22,8 +21,8 @@ module.exports = function (config) {
     webpackServer: {
       noInfo: true
     },
-    reporters: isTestWatch ? ['mocha']: ['mocha', 'coverage'],
-    coverageReporter: isTestWatch ? undefined : {
+    reporters: ['mocha', 'coverage'],
+    coverageReporter: {
       dir: 'coverage/',
       reporters: [{
         type: 'json',
@@ -36,7 +35,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: isTestWatch ? ['Chrome'] : ['PhantomJS'], 
+    browsers: ['PhantomJS'], 
     singleRun: true
    });
 };

configs/webpack.test.js
@@ -30,11 +30,12 @@ module.exports = {
       },
       {
         test: /\.ts$/,
-        loaders: ['awesome-typescript-loader?inlineSourceMap=true&sourceMap=false', 'angular2-template-loader']
+        loaders: ['awesome-typescript-loader', 'angular2-template-loader']
       },
       {test: /\.css$/, loader: 'raw-loader'},
       {test: /\.html$/, loader: 'raw-loader'},
       // {
+      //   enforce: 'post',
       //   test: /\.ts$/,
       //   include: helpers.path.resolve('src'),
       //   loader: 'istanbul-instrumenter-loader',

src/app/components/signup/signup.component.spec.ts
@@ -78,15 +78,15 @@ describe('SignupComponent', () => {
   it('can validate inputs', () => {
     const page: SignupComponent = cmpDebugElement.componentInstance;
     page.username.setValue('a');
-    page.firstname.setValue('fn');
-    page.lastname.setValue('ln');
+    page.firstname.setValue('firstname');
+    page.lastname.setValue('lastname');
     page.email.setValue('b', {});
     page.password.setValue('c', {});
     page.passwordConfirmation.setValue('d', {});
     expect(page.myForm.valid).toBeFalsy();
     page.username.setValue('akira', {});
-    page.firstname.setValue('fn');
-    page.lastname.setValue('ln');
+    page.firstname.setValue('firstname');
+    page.lastname.setValue('lastname');
     page.email.setValue('test@test.com', {});
     page.password.setValue('secret123', {});
     page.passwordConfirmation.setValue('secret123', {});