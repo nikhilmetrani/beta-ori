668def0b53f0234587058f6bcea23ecc90cc5b79
Nikhil Metrani <nikhilmetrani@u.nus.edu>
2016-10-31 22:25:02 +0800

    Fix for bug #13
    Fixed by adding call to CopyWebpackPlugin() in webpack.dev.js and webpack.aot.js
    The call enable webpack to copy static resources into dist folder.
    If any additional atatic resources are addeded to the project, they must be listed
    in the CopyWebpackPlugin() call to enable copying.

--- configs/webpack.aot.js
+++ configs/webpack.aot.js
@@ -2,6 +2,7 @@ const helpers = require('./helpers');
 const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
 const PUBLIC_PATH = (process.env.PUBLIC_PATH || '') + '/' ;
@@ -44,6 +45,22 @@ module.exports = webpackMerge(commonConfig, {
       comments: false,
       sourceMap: true
     }),
+    new CopyWebpackPlugin(
+      [
+        { // Copy bootstrap fonts to dist/fonts folder
+          from: 'node_modules/bootstrap/dist/fonts',
+          to: 'fonts'
+        },
+        { // Copy images / icons
+          from: 'src/public/img',
+          to: 'img'
+        },
+        { // Copy error page
+          from: 'src/public/error',
+          to: 'error'
+        },
+      ]
+    ),
   ],
   node: {
     global: true,
--- configs/webpack.dev.js
+++ configs/webpack.dev.js
@@ -2,6 +2,7 @@ const helpers = require('./helpers');
 const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const ENV = process.env.ENV = process.env.NODE_ENV = 'development';
 
@@ -27,6 +28,22 @@ module.exports = webpackMerge(commonConfig, {
         },
       },
     }),
+    new CopyWebpackPlugin(
+      [
+        { // Copy bootstrap fonts to dist/fonts folder
+          from: 'node_modules/bootstrap/dist/fonts',
+          to: 'fonts'
+        },
+        { // Copy images / icons
+          from: 'src/public/img',
+          to: 'img'
+        },
+        { // Copy error page
+          from: 'src/public/error',
+          to: 'error'
+        },
+      ]
+    ),
   ],
   devServer: {
     port: 4200,
